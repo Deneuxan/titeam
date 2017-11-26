3eeeee430da838138dd4333f12a005feae67f859
María Clara BEZZECCHI <bezouki@mc.bezouki-VM>
2017-11-26 05:06:18 +0100

    Deconnexion, etc

--- carGeneration/urls.py
+++ carGeneration/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^inscription$', views.inscription, name="inscription"),
+	url(r'^desinscription$', views.desinscription, name="desinscription"),
 	url(r'^connexion$', views.connexion, name="connexion"),
+	url(r'^deconnexion$', views.deconnexion, name="deconnexion"),
+	url(r'^accueil$', views.accueil, name="accueil"),
 ]
